4e4a2d568cbfac9f0cd565c01dcf6337762c5b17
ferhatgec <ferhat2gecdogan_fix_me5@protonmail.com>
2021-03-25 20:43:45 +0300

    Add: GetFirstArgument function and code cleaning.

--- example.cpp
+++ example.cpp
@@ -8,10 +8,11 @@
 #include "StringTools.hpp"
 
 int main() {
-	/*std::string betweenstr("#Hello developers!?");
-	std::cout << "String between # and ? : " << stringtools::GetBetweenString(betweenstr, "#", "?") << "\n";
-	std::cout << "L character count : " << stringtools::Count(betweenstr, 'l') << "\n"; */
-	int a = stringtools::Counter("Hello StringTools! 1234!@#)_qASD)(#@)$dla:cma><)_", 1);
-	std::cout << a;
+	// std::string betweenstr("#Hello developers!?");
+	// std::cout << "String between # and ? : " << stringtools::GetBetweenString(betweenstr, "#", "?") << "\n";
+	// std::cout << "L character count : " << stringtools::Count(betweenstr, 'l') << "\n"; */
+
+	std::cout << stringtools::Counter("Hello StringTools! 1234!@#)_qASD)(#@)$dla:cma><)_", 1) << '\n';
+
 	return 0;
 }
